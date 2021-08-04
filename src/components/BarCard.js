8c48a9aba0f40ecc6c5424fea85b6800663cec66
Henry <hhpendleton@gmail.com>
2021-08-04 10:48:57 -0400

    small edits

--- src/components/BarCard.js
+++ src/components/BarCard.js
@@ -5,9 +5,6 @@ import styled from "styled-components";
 const Card = styled.div`
   padding: 50px;
 `;
-const ChartBox = styled.div`
-  height: 300px;
-`;
 
 const BarCard = ({ bar }) => {
   const position = [bar.latitude.toFixed(2), bar.longitude.toFixed(2)];
@@ -22,25 +19,6 @@ const BarCard = ({ bar }) => {
       {bar.bar_game_types.map((type) => (
         <div>{type.game_type}</div>
       ))}
-
-      <ChartBox>
-        {/* <MapContainer
-          className="map-container"
-          center={position}
-          zoom={5}
-          scrollWheelZoom={false}
-        >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer> */}
-      </ChartBox>
     </Card>
   );
 };
